966626ff30fca426c34fa6f2f8cc9fd09170f5af
Zoë Bryant <zoebryant@Zoes-MacBook-Pro.local>
2016-05-27 18:09:51 +0100

    Basic tests for Deploy button

--- app/components/Deploy/Deploy-test.js
+++ app/components/Deploy/Deploy-test.js
@@ -2,20 +2,39 @@ import { expect } from 'chai';
 import React from 'react';
 import Deploy from './Deploy.js';
 import { mount, shallow } from 'enzyme';
-
+import sinon from 'sinon';
+import fetchMock from 'fetch-mock';
 
 describe.only('Deploy button', () => {
 
+  beforeEach(() => {
+    fetchMock.mock('/staging', 200)
+  });
+
+  afterEach(() => {
+    fetchMock.restore
+  });
+
   describe('for staging', () => {
-    const wrapper = shallow(<Deploy environment="staging" />);
 
     it('Renders a button labeled staging', () => {
+      const wrapper = shallow(<Deploy environment="staging" />);
       const button = wrapper.find('button')
       expect(button).to.have.length(1);
-
-      const buttonContent = button.node.props.children;
-      expect(buttonContent.search("staging")).not.to.equal(-1)
+      expect(wrapper.text()).to.contain("staging");
     });
+
+    it.only('calls /staging when clicked', () => {
+
+      const wrapper = shallow(
+        <Deploy />
+      );
+      wrapper.find('button').simulate('click');
+
+      expect(fetchMock.called('/staging')).to.be.true;
+      expect(fetchMock.lastOptions('/staging').method).to.eq('POST');
+      // expect(fetchMock.lastOptions('/staging').body).to.eq(content);
+    })
   })
 
   describe('for live', () => {
@@ -24,9 +43,7 @@ describe.only('Deploy button', () => {
     it('Renders a button labeled live', () => {
       const button = wrapper.find('button')
       expect(button).to.have.length(1);
-
-      const buttonContent = button.node.props.children;
-      expect(buttonContent.search("live")).not.to.equal(-1)
+      expect(wrapper.text()).to.contain("live");
     });
   })
 
--- app/components/Deploy/Deploy.js
+++ app/components/Deploy/Deploy.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 import Radium from 'radium';
 
 class Deploy extends Component {
+
+  handleClick() {
+    fetch('/staging', {
+      method: 'POST',
+    });
+  }
+
   render() {
     return (
-      <button>{this.props.environment}</button>
+      <button onClick={this.handleClick}>{this.props.environment}</button>
     )
   }
 };
 
 export default Deploy;
+
+
+
+// handleClick
+// takes the contnet and the desired env
+// fires push to External
+// which fires a fetch...
